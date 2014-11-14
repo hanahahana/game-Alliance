2ddd8763a82e35afd2f22ed2849313b3e3948683
Michael Allen <michael.allen@cognex.com>
2014-11-14 12:34:25 -0600

    Initial commit

---
+++ src/Alliance/AllianceGame.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SharpDX;
+using SharpDX.Toolkit;
+using SharpDX.Toolkit.Graphics;
+using SharpDX.Toolkit.Input;
+
+namespace Alliance
+{
+  public class AllianceGame : Game
+  {
+    private GraphicsDeviceManager graphics;
+
+    public AllianceGame()
+    {
+      graphics = new GraphicsDeviceManager(this);
+      Content.RootDirectory = "Content";
+    }
+
+    protected override void Initialize()
+    {
+      IsMouseVisible = true;
+      Window.Title = "Alliance";
+
+      graphics.PreferredBackBufferWidth = 800;
+      graphics.PreferredBackBufferHeight = 600;
+      graphics.ApplyChanges();
+
+      base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+      base.LoadContent();
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+      base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+      GraphicsDevice.Clear(ClearOptions.Target | ClearOptions.DepthBuffer, Color.CornflowerBlue, 1f, 0);
+      base.Draw(gameTime);
+    }
+  }
+}
---
+++ src/Alliance/Resources/ResourceCache.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX.Toolkit.Content;
+using SharpDX.Toolkit.Graphics;
+
+namespace Alliance
+{
+  public class ResourceCache
+  {
+    public AllianceGame Owner { get; private set; }
+    public ContentManager Content { get { return Owner.Content; } }
+
+    public Dictionary<string, FramedImage> Images { get; private set; }
+    public Dictionary<string, SpriteFont> Fonts { get; private set; }
+    public Dictionary<string, Texture2D> Textures { get; private set; }
+    //public static GridComponent CurrentGrid { get; private set; }
+
+    public ResourceCache(AllianceGame owner)
+    {
+      Owner = owner;
+      LoadContent();
+    }
+
+    private T Load<T>(string p)
+    {
+      return Content.Load<T>(p);
+    }
+
+    private void LoadContent()
+    {
+      LoadFonts();
+      LoadImages();
+      LoadTextures();
+    }
+
+    private void LoadTextures()
+    {
+      Textures = new Dictionary<string, Texture2D>();
+      Textures["background"] = Load<Texture2D>(@"Textures\background");
+      Textures["blank"] = Load<Texture2D>(@"Textures\blank");
+      Textures["gradient"] = Load<Texture2D>(@"Textures\gradient");
+    }
+
+    private void LoadFonts()
+    {
+      Fonts = new Dictionary<string, SpriteFont>();
+      Fonts["ComicSans"] = Load<SpriteFont>(@"Fonts\ComicSans");
+      Fonts["Tahoma"] = Load<SpriteFont>(@"Fonts\Tahoma");
+      Fonts["Verdana"] = Load<SpriteFont>(@"Fonts\Verdana");
+      Fonts["Georgia"] = Load<SpriteFont>(@"Fonts\Georgia");
+      Fonts["Arial"] = Load<SpriteFont>(@"Fonts\Arial");
+      Fonts["BookmanOldStyle"] = Load<SpriteFont>(@"Fonts\BookmanOldStyle");
+      Fonts["Rockwell"] = Load<SpriteFont>(@"Fonts\Rockwell");
+    }
+
+    private void LoadImages()
+    {
+      FramedImage[] images = new FramedImage[]
+      {
+        // bases
+        new FramedImage(Content, @"Images\Bases", "towerBase", false, 0, 0),
+
+        // enemies ( all enemies are animated to avoid exceptions )
+        new FramedImage(Content, @"Images\Enemies", "walker", true, 4, 1),
+        new FramedImage(Content, @"Images\Enemies", "biker", true, 4, 1),
+        new FramedImage(Content, @"Images\Enemies", "flapper", true, 5, 1),
+        new FramedImage(Content, @"Images\Enemies", "glider", true, 4, 1),
+
+        // towers
+        new FramedImage(Content, @"Images\Towers", "railgun", false, 0, 0),
+        new FramedImage(Content, @"Images\Towers", "turret", false, 0, 0),
+        new FramedImage(Content, @"Images\Towers", "missileLauncher", false, 0, 0),
+        new FramedImage(Content, @"Images\Towers", "shockwaveGenerator", false, 0, 0),
+        new FramedImage(Content, @"Images\Towers", "speedbump", false, 0, 0),
+        new FramedImage(Content, @"Images\Towers", "sprinkler", false, 0, 0),
+        new FramedImage(Content, @"Images\Towers", "teslaCoil", true, 5, 1),
+        new FramedImage(Content, @"Images\Towers", "machinegun", false, 0, 0),
+        new FramedImage(Content, @"Images\Towers", "flamethrower", false, 0, 0),
+        new FramedImage(Content, @"Images\Towers", "seeker", false, 0, 0),
+
+        // projectiles
+        new FramedImage(Content, @"Images\Projectiles", "rocket", false, 0, 0),
+        new FramedImage(Content, @"Images\Projectiles", "bullet", false, 0, 0),
+        new FramedImage(Content, @"Images\Projectiles", "pulse", false, 0, 0),
+        new FramedImage(Content, @"Images\Projectiles", "debri", false, 0, 0),
+        new FramedImage(Content, @"Images\Projectiles", "fragment", false, 0, 0),
+        new FramedImage(Content, @"Images\Projectiles", "lightning", false, 0, 0),
+        new FramedImage(Content, @"Images\Projectiles", "flame", false, 0, 0),
+        new FramedImage(Content, @"Images\Projectiles", "flamewave", false, 0, 0),
+        new FramedImage(Content, @"Images\Projectiles", "wave", false, 0, 0),
+        new FramedImage(Content, @"Images\Projectiles", "lockmissile", false, 0, 0),
+      };
+
+      Images = images.ToDictionary(k => k.Key, v => v);
+    }
+  }
+}
